6fb8307371d1a2b9c91683d77373231c3ab7dc2d
Nghia DX <nghiaseo1111988@gmail.com>
2024-04-05 22:31:38 +0700

    asd

--- authservice/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from authservice import views
-
-urlpatterns = [
-    path('register/', views.RegisterView.as_view(), name='register'),
-
-]
-# Compare this snippet from authservice/views.py:
--- cinema/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('',views.showing,name='showing'),
-    path('movie/',views.index,name='index'),
-    path('book/',views.book,name='book'),
-]
